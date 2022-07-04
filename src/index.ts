54491ea13e6eac047362ec79314227a191c39033
Sergen Kovar <sergen.kovar@octo.com>
2022-07-04 15:08:54 +0200

    Repond HelloWorld

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
+import Http from 'http'
+import { servicesVersion } from 'typescript'
+
 export function createServer(){
+    const server = Http.createServer((req, res) => {
+        res.write('Bonjour')
+        res.end()
+    })
     return {
-        start(){
-            
+        async start() {
+            return new Promise<void>((ok, ko) => {
+                // @ts-ignore
+                server.listen(8888, (error) => {
+                    if (error) {
+                        ko(error)
+                    } else {
+                        ok()
+                    }
+                })
+            })
+        },
+        async stop() {
+            return new Promise<void>((ok, ko) => {
+                server.close((error) => {
+                    if (error) {
+                        ko(error)
+                    } else {
+                        ok()
+                    }
+                })
+            })
         }
     }
 }

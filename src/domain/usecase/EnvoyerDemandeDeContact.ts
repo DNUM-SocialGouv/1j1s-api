796c7f541e2dcc15af07d1b6ca0a2b36dbe06093
Florent Jaby <florent.jaby@gmail.com>
2022-07-06 17:11:04 +0200

    Enregistrer une demande de contact dans le dépôt depuis le usecase

--- src/domain/usecase/EnvoyerDemandeDeContact.ts
+++ src/domain/usecase/EnvoyerDemandeDeContact.ts
@@ -1,7 +1,17 @@
 import { v4 as uuidv4 } from 'uuid'
+import { DemandeDeContact } from '../model/DemandeDeContact'
+import { DemandeDeContactRepository } from '../../infra/DemandeDeContactRepository'
 
 export class EnvoyerDemandeDeContact {
+  private readonly repository: DemandeDeContactRepository
+
+  constructor (repository: DemandeDeContactRepository) {
+    this.repository = repository
+  }
+
 	public async execute () : Promise<string> {
-		return uuidv4()
+    const demande: DemandeDeContact = { id: uuidv4() }
+    await this.repository.save(demande)
+    return demande.id
 	}
 }
--- test/domain/usecase/EnvoyerDemandeDeContactTest.ts
+++ test/domain/usecase/EnvoyerDemandeDeContactTest.ts
@@ -1,6 +1,7 @@
 import { EnvoyerDemandeDeContact } from '../../../src/domain/usecase/EnvoyerDemandeDeContact'
-import chai from 'chai'
+import { DemandeDeContactRepository } from '../../../src/infra/DemandeDeContactRepository'
 import ChaiUuid from 'chai-uuid'
+import chai from 'chai'
 chai.use(ChaiUuid)
 const expect = chai.expect
 
@@ -8,17 +9,31 @@ describe('EnvoyerDemandeDeContact', () => {
 	describe('.execute()', () => {
 		it('résoud sans erreur', async () => {
 			// GIVEN
-			const envoyerDemandeDeContact = new EnvoyerDemandeDeContact()
+      const repository = new DemandeDeContactRepository()
+			const envoyerDemandeDeContact = new EnvoyerDemandeDeContact(repository)
 			// WHEN
 			await envoyerDemandeDeContact.execute()
 		})
+
 		it('résoud un uuid', async () => {
 			// GIVEN
-			const envoyerDemandeDeContact = new EnvoyerDemandeDeContact()
+      const repository = new DemandeDeContactRepository()
+			const envoyerDemandeDeContact = new EnvoyerDemandeDeContact(repository)
 			// WHEN
 			const actual = await envoyerDemandeDeContact.execute()
 			// THEN
-			expect(actual).to.be.a.uuid('v4') // @ts-ignore
+			expect(actual).to.be.a.uuid('v4')
 		})
+
+    it('enregistre dans le dépôt à demande de contacts', async () => {
+			// Given
+      const repository = new DemandeDeContactRepository()
+			const envoyerDemandeDeContact = new EnvoyerDemandeDeContact(repository)
+			// When
+			const id = await envoyerDemandeDeContact.execute()
+      // Then
+      const saved = await repository.get(id)
+      expect(saved).to.deep.equal({ id })
+    })
 	})
 })
